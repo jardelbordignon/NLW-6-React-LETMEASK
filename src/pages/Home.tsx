f4697ddc96bc18c8bb73840df48746f929e4d19c
Jardel <jardel1101@gmail.com>
2021-06-22 16:36:11 -0300

    Autenticação com firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { useHistory } from 'react-router-dom'
 
+import { auth, firebase } from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -11,8 +13,13 @@ import '../styles/auth.scss'
 export function Home() {
   const history = useHistory()
 
-  function navToNewRoom() {
-    history.push('/rooms/new')
+  function handleCreateRoom() {
+    const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
+
+    auth.signInWithPopup(googleAuthProvider).then(result => {
+      console.log(result)
+      history.push('/rooms/new')
+    })
   }
 
   return (
@@ -26,7 +33,7 @@ export function Home() {
       <main>
         <div className='main-content'>
           <img src={logoImg} alt='Logo da Letmeask' />
-          <button onClick={navToNewRoom} className='create-room'>
+          <button onClick={handleCreateRoom} className='create-room'>
             <img src={googleIconImg} alt='Logo da Google' />
             Crie sua sala com o Google
           </button>
